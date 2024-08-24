df7924c91eaddfabcc5d12dd66b27dd32b7e5eec
Oliver Wackenreuther <166775914+Owack@users.noreply.github.com>
2024-08-24 12:26:49 -0700

    Add project files.

---
+++ BricsCAD Batch v24/Class1.cs
@@ -0,0 +1,16 @@
+﻿using Bricscad.ApplicationServices;
+using Bricscad.Runtime;
+using Teigha.Runtime;
+
+namespace BricsCADDialogLibrary
+{
+    public class BricsCADDialog
+    {
+        [CommandMethod("OW:BatchScripter")]
+        public void BatchScripter()
+        {
+            MainForm form = new MainForm();
+            Application.ShowModalDialog(form);
+        }
+    }
+}
